54225e0723e9aae149f22edc935a89c15683012c
mydndb13@naver.com <mydndb13@naver.com>
2018-12-01 20:13:10 +0900

    tes

--- src/main/java/com/example/demo/repository/impl/AdminRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/impl/AdminRepositoryImpl.java
@@ -14,7 +14,7 @@ import com.example.demo.vo.AdminInfoVO;
 public class AdminRepositoryImpl implements AdminRepository {
 	
 	@Autowired
-	@Qualifier("sqlSessionTemplate")
+	//@Qualifier("sqlSessionTemplate")
 	private SqlSession ss;
 
 	@Override
